038ab626e16ad44ed56470de47149d93e826236d
Balaji Krishnamurthy <balaji-kmty@Shubhas-MacBook-Pro.local>
2019-04-06 17:15:24 -0700

    adding solutions to variables chapter exercises

---
+++ variables/name.rb
@@ -0,0 +1,14 @@
+# Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included.
+puts "Enter your name: "
+name = gets.chomp
+puts "Greetings, " + name + "!"
+
+# Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row.
+10.times { puts name }
+
+# Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
+puts "Enter your first name: "
+first_name = gets.chomp
+puts "Enter your last name: "
+last_name = gets.chomp
+puts first_name + " " + last_name
